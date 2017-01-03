72386d9c33a909ca7d2958463894b232a7b318b5
Andrii Sergiienko <andrey.sergienko@gmail.com>
2017-01-03 22:41:32 +0200

    update vim

--- .zshrc.sh
+++ .zshrc.sh
@@ -21,9 +21,11 @@ prompt 'paulmillr'
 # = Aliases =
 # ==================================================================
 
+export FINDBUGS_HOME=/Users/andreysergienko/Library/Application\ Support/IntelliJIdea2016.2/FindBugs-IDEA-1.0.0/lib
+
 alias ruby=/usr/local/Cellar/ruby/2.3.1/bin/ruby
-alias vi=/usr/local/Cellar/vim/7.4.2235/bin/vim
-alias vim=/usr/local/Cellar/vim/7.4.2235/bin/vim
+alias vi=/usr/local/Cellar/vim/8.0.0142/bin/vim
+alias vim=/usr/local/Cellar/vim/8.0.0142/bin/vim
 alias sed=gsed
 
 alias -g f10='| head -n 10'
@@ -39,6 +41,32 @@ alias lint=jshint
 
 # Faster NPM for europeans.
 alias npme='npm --registry http://registry.npmjs.eu'
+
+alias maven="command mvn"
+function color_maven() {
+    local BLUE="\x1b[0;34m"
+    local RED="\x1b[0;31m"
+    local GREEN="\x1b[0;32m"
+    local YELLOW="\x1b[1;33m"
+    local WHITE="\x1b[1;37m"
+    local LIGHT_RED="\x1b[1;31m"
+    local LIGHT_GREEN="\x1b[1;32m"
+    local LIGHT_BLUE="\x1b[1;34m"
+    local LIGHT_CYAN="\x1b[1;36m"
+    local NO_COLOUR="\x1b[0m"
+    maven $* | gsed \
+        -e "s/Tests run: \([^,]*\), Failures: \([^,]*\), Errors: \([^,]*\), Skipped: \([^,]*\)/${LIGHT_GREEN}Tests run: \1$NO_COLOUR, Failures: $RED\2$NO_COLOUR, Errors: $YELLOW\3$NO_COLOUR, Skipped: $LIGHT_BLUE\4$NO_COLOUR/g" \
+        -e "s/\(\[\{0,1\}WARN\(ING\)\{0,1\}\]\{0,1\}.*\)/$YELLOW\1$NO_COLOUR/g" \
+        -e "s/\(\[ERROR\].*\)/$RED\1$NO_COLOUR/g" \
+        -e "s/\(\(BUILD \)\{0,1\}FAILURE.*\)/$RED\1$NO_COLOUR/g" \
+        -e "s/\(\(BUILD \)\{0,1\}SUCCESS.*\)/$LIGHT_GREEN\1$NO_COLOUR/g" \
+        -e "s/\(\[INFO\].*\)/$GREEN\1$NO_COLOUR/g"
+
+        MAVEN_STATUS=$PIPESTATUS
+
+        return $MAVEN_STATUS
+}
+alias mvn=color_maven
 
 # Some OS X-only stuff.
 if [[ "$OSTYPE" == darwin* ]]; then
