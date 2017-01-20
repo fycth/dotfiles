4d7324f9e0f4b7dd5d36bcee46d6961d1512bf2a
Andrii Sergiienko <andrey.sergienko@gmail.com>
2017-01-20 22:27:37 +0200

    fix zsh history; move exports to env from rc

--- .zshrc.sh
+++ .zshrc.sh
@@ -21,7 +21,8 @@ prompt 'paulmillr'
 # = Aliases =
 # ==================================================================
 
-export FINDBUGS_HOME=/Users/andreysergienko/Library/Application\ Support/IntelliJIdea2016.2/FindBugs-IDEA-1.0.0/lib
+
+alias history='history -i'
 
 alias ruby=/usr/local/Cellar/ruby/2.3.1/bin/ruby
 alias vi=/usr/local/Cellar/vim/8.0.0142/bin/vim
@@ -393,10 +394,3 @@ function preview() {
   [[ -z "$item" ]] && item='.'
   open $1 -a 'Preview'
 }
-
-export HISTTIMEFORMAT="%d/%m/%y %T "
-
-# docker-osx-dev
-export DOCKER_HOST=tcp://192.168.99.100:2376
-export DOCKER_CERT_PATH=/Users/andreysergienko/.docker/machine/machines/default
-export DOCKER_TLS_VERIFY=1
